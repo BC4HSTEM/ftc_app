6da021f2baeb4adb80e1bf9ea79c47deb08aa71b
Danny McCaslin <mccaslin.danny@gmail.com>
2018-11-04 10:07:17 -0500

    Added instruction comment regarding motor direction and generalized the Drive method. Added instructions for driving forwards and backwards. Framed turning methods.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTutorial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTutorial.java
@@ -1,13 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-
-@TeleOp(name = "Tele-Op Tutorial", group = "Tutorials")
-public class TeleopTutorial extends LinearOpMode {
+/*Autonompus Code:
+This code is assuming that the left motor is mounted backwards. If the right motor is mounted
+backwards, commend out the line motorLeft.setDirection(DcMotor.Direction.REVERSE); and un-commend
+the line motorRight.setDirection(DcMotor.Direction.REVERSE);
+*/
+@Autonomous(name = "Tele-Op Tutorial", group = "Tutorials")
+public class AutonomousTutorial extends LinearOpMode {
 
     private DcMotor motorLeft;
     private DcMotor motorRight;
@@ -16,6 +19,7 @@ public class TeleopTutorial extends LinearOpMode {
 
     private static final double ARM_RETRACTED_POSITION = 0.2;
     private static final double ARM_EXTENDED_POSITION = 0.8;
+    private static final int ENCODER_CLICKS = 2240;
 
     @Override
     public void runOpMode() throws InterruptedException
@@ -25,6 +29,12 @@ public class TeleopTutorial extends LinearOpMode {
 
         //one of the motors will be mounted backwards, so it should be reversed.
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        //motorRight.setDirection(DcMotor.Direction.REVERSE);
+
+
+        motorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
 
         armServo = hardwareMap.servo.get("armServo");
 
@@ -33,23 +43,43 @@ public class TeleopTutorial extends LinearOpMode {
         //code before waitForStart is run when Init button is pressed
         waitForStart();
         //code after waitForStart is run when the start button is pressed
-
-        while(opModeIsActive())
-        {
-
-            motorLeft.setPower(gamepad1.left_stick_y);
-            motorRight.setPower(gamepad1.right_stick_y);
-
-            if(gamepad2.a)
-            {
-                armServo.setPosition(ARM_EXTENDED_POSITION);
-            }
-            if(gamepad2.b)
-            {
-                armServo.setPosition(ARM_RETRACTED_POSITION);
-            }
-
-            idle();
-        }
+        Drive(-30, -0.8);
     }
+
+    public static int getEncoderClicks (double distance) // Distance in centimeters
+     {
+        //Wheel diameter in centimeters,
+        double wheelDiam = 10.0;
+        double wheelCircum = Math.PI * wheelDiam;
+        double clicksPerCm = ENCODER_CLICKS / wheelCircum;
+        int outputClicks = (int)Math.floor(clicksPerCm * distance);
+
+        return outputClicks;
+    }
+
+    public void Drive( double distance, double power)
+    {
+        /* Entering positive values into distance and power will drive the robot forward. Entering
+        negative values will drive the robot backward.
+         */
+        motorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorLeft.setPower(power);
+        motorRight.setPower(power);
+        motorLeft.setTargetPosition(getEncoderClicks(distance));
+        motorRight.setTargetPosition(getEncoderClicks(distance));
+
+    }
+
+    public void TurnLeft( double degrees, double power)
+    {
+
+    }
+
+    public void TurnRight(double degree, double power) {
+
+    }
+
+
+
 }
